534515d93f402bd100972bef51c55c252a8c6fc9
Nicol <nicoletagologan@yahoo.com>
2019-04-02 19:40:52 +0200

    EventPage+SubMenu+Calendario

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,18 @@
 import 'bootstrap';
 
 import '../css/app.scss';
+import '../js/jsCalendar.min.js';
 
 
-	
+// Get the element
+var element = document.getElementById("my-calendar");
+// Create the calendar
+jsCalendar.new(element,"02/04/2019");
 
+jsCalendar.onDateClick(function(event, date){
+    // On day click
+  });
+  
+  jsCalendar.onMonthChange(function(event, date){
+    // On month change
+  });
